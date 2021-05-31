03c44e75c07ecc30b80de318445415f2dddb7b21
js0753 <jainam02@somaiya.edu>
2021-05-31 17:28:55 +0530

    Telephone Dir file r/w

--- telephone_prg/main.go
+++ telephone_prg/main.go
@@ -7,25 +7,34 @@ func main() {
 
 	c := 0
 	for i := 0; i > -1; i++ {
-		fmt.Println("Choose \n1.Add\n2.Update\n3.Search\n4.Find Duplicates\n5.Delete\n6.Print Directory\n7.Exit")
+		fmt.Println("Choose \n1.Add\n2.Update\n3.Search\n4.Find Duplicates\n5.Delete\n6.Print Directory\n7.Load Directory from File\n8.Exit")
 		fmt.Scanln(&c)
 		if c == 1 {
-			var name string
-			var number int
-			fmt.Println("Enter Name and number : ")
-			fmt.Scanln(&name, &number)
-			td.add(name, number)
+			for k := 0; k > -1; k++ {
+				var name string
+				var number int
+				var email string
+				var address string
+				fmt.Println("Enter Name, number ,email and address : ")
+
+				_, err := fmt.Scanln(&name, &number, &email, &address)
+				if err != nil {
+					//fmt.Println(err)
+					break
+				}
+				td.add(name, number, email, address)
+			}
 		} else if c == 2 {
 			var name string
 			var number int
-			fmt.Println("Enter Name and updated number : ")
+			fmt.Println("Enter Name and updated number,email,address : ")
 			fmt.Scanln(&name, &number)
 			td.update(name, number)
 		} else if c == 3 {
 			var name string
 			fmt.Println("Enter Name : ")
 			fmt.Scanln(&name)
-			td.search(name)
+			td.searchByName(name)
 		} else if c == 4 {
 			td.find_dup()
 		} else if c == 5 {
@@ -35,6 +44,8 @@ func main() {
 			td.delete(name)
 		} else if c == 6 {
 			td.print()
+		} else if c == 7 {
+			td = TelDirFromFile("teldir")
 		} else {
 			break
 		}
@@ -44,10 +55,11 @@ func main() {
 
 /*
 
-Eren 9823346162
-Zeke 9823346162
-Mikasa 6825546164
-Levi 6451184626
-Jean 4627719336
-Sasha 5182039448
+Eren 9823346162 eren@gmail.com Mumbai
+Zeke 9823346162 zeke@gmail.com Pune
+Mikasa 6825546164 mikasa@gmail.com Delhi
+Levi 6451184626 levi@gmail.com Chennai
+Jean 4627719336 jean@gmail.com Kolkata
+Sasha 5182039448 sasha@gmail.com Mumbai
+-1
 */
--- telephone_prg/teldir.go
+++ telephone_prg/teldir.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
 //type teldir map[int]Entry
 type Entry struct {
@@ -10,47 +16,107 @@ type Entry struct {
 	address string
 }
 
-func (t *teldir) add(name string, number int) {
-	if (*t)[name] == 0 {
-		(*t)[name] = number
-	} else {
-		fmt.Println("Entry Already Exists")
+type teldir []Entry
+
+func TelDirFromFile(filename string) teldir {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		// opt1 - log err and return call to newDeck()
+		// opt2 - log err and quit program
+		fmt.Println("ERROR:", err)
+		os.Exit(1)
+
 	}
-}
-func (t *teldir) update(name string, number int) {
-	(*t)[name] = number
-}
-func (t *teldir) delete(name string) {
-	delete(*t, name)
+	var temp_td teldir
+	s := strings.Split(string(bs), "\n")
+	for _, row := range s {
+		values := strings.Split(row, ",")
+		if len(values) > 1 {
+			var e Entry
+			//fmt.Println(values[2])
+			e.name = values[0]
+			e.number, _ = strconv.Atoi(values[1])
+			e.email = values[2]
+			e.address = values[3]
+			temp_td = append(temp_td, e)
+		}
+	}
+	//temp_es.print()
+	return temp_td
 }
 
-func (t teldir) search(name string) {
-	for nm, num := range t {
-		if nm == name {
-			fmt.Println(nm+"'s Number is : ", num)
+func (t *teldir) add(name string, number int, email string, address string) {
+	var e Entry
+	e.name = name
+	e.number = number
+	e.email = email
+	e.address = address
+	*t = append(*t, e)
+	e.addToFile("teldir")
+}
+
+func (t teldir) saveToFile(filename string) error {
+	main_string := ""
+	for _, row := range t {
+		row_string := row.name + "," + fmt.Sprint(row.number) + "," + row.email + "," + row.address
+		// conversion from int to string yields a string of one rune, not a string of digits (did you mean fmt.Sprint(x)?)
+		main_string = main_string + row_string + "\n"
+	}
+	return os.WriteFile(filename, []byte(main_string), 0666)
+}
+
+func (e Entry) addToFile(filename string) error {
+	entry_string := e.name + "," + fmt.Sprint(e.number) + "," + e.email + "," + e.address
+	f, err := os.OpenFile(filename,
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644) //  0644 == rw-r-r //os.O_WRONLY is to state the file mode, wether read,write, or both, in this case write
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+	_, write_err := f.WriteString(entry_string + "\n")
+
+	return write_err
+}
+
+func (t *teldir) update(name string, number int) {
+	for i, entry := range *t {
+		if entry.name == name {
+			(*t)[i].number = number
+		}
+	}
+	(*t).saveToFile("teldir")
+}
+func (t *teldir) delete(name string) {
+	//delete(*t, name)
+	for i, entry := range *t {
+		if entry.name == name {
+			*t = append((*t)[:i], (*t)[i+1:]...) // ... lets you pass multiple arguments to a function from a slice. Here append function is called recursively for each element in (*t)[i+1:]
+			break
+		}
+	}
+	(*t).saveToFile("teldir")
+}
+
+func (t teldir) searchByName(name string) {
+	for _, entry := range t {
+		if entry.name == name {
+			fmt.Println(entry.name+"'s Number is : ", entry.number)
 		}
 	}
 }
 
 func (t teldir) print() {
 	fmt.Println("Numbers in Directory : ")
-	for name, num := range t {
-		fmt.Println(name, num)
+	for _, entry := range t {
+		fmt.Println(entry.name, entry.number, entry.email, entry.address)
 	}
 }
 
 func (t teldir) find_dup() {
-	var entry_list []Entry
-	for name, num := range t {
-		e := new(Entry)
-		e.name = name
-		e.number = num
-		entry_list = append(entry_list, *e)
-	}
-	for i, ent := range entry_list {
+	for i, ent := range t {
 		for j := 0; j < i; j++ {
-			if ent.number == entry_list[j].number {
-				fmt.Println("Duplicate Entries Found  : \n", ent.name, ent.number, "\n", entry_list[j].name, entry_list[j].number)
+			if ent.number == t[j].number {
+				fmt.Println("Duplicate Entries Found  : \n", ent.name, ent.number, "\n", t[j].name, t[j].number)
 			}
 		}
 	}
